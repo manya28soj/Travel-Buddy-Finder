1faaed67dbc67bd881357d8709b9866c2bf6e23d
Manya <144474092+manya28soj@users.noreply.github.com>
2026-06-06 09:52:40 +0530

    Add files via upload

---
+++ middleware.js
@@ -0,0 +1,10 @@
+{
+  "name": "travel-buddy-finder",
+  "version": "1.0.0",
+  "private": true,
+  "scripts": {
+    "client": "npm run dev --prefix client",
+    "server": "npm run dev --prefix server",
+    "install:all": "npm install --prefix client && npm install --prefix server"
+  }
+}
